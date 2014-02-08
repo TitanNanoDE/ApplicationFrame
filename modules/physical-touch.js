22a9138675f1d608e5fc3749607d52757ef2ab54
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2014-02-09 00:19:00 +0100

    physical touch refactoring
    refactoring of the scrolling process. Scrolling and overscroling works
    now.

--- modules/physical-touch.js
+++ modules/physical-touch.js
@@ -23,73 +23,119 @@ $('escape').wrapper(function(){
 //  physical touch meta data
     var touchMeta= [];
     var overScrollMeta= [];
+
+/*  ---     Classes     --- */
+    
+    var EventObjectExtension= function(index){
+/*      new properties are:
+            event.timeout
+            event.speed
+            event.movement          */            
+        var now= new Date().getTime();
+        this.timeout= (now - touchMeta[index].lastTime) / 1000;
+        
+        if(this.timeout === 0){
+            this.timeout= touchMeta[index].lastTimeout;
+        }else{
+            touchMeta[index].lastTimeout= this.timeout;
+            }
+        this.position= { x: this.screenX, y: this.screenY };
+        
+        if( (touchMeta[index].lastPos.y != this.position.y && touchMeta[index].lastMovement.x != this.position.x) || (this.timeout > 0.1) ){
+            this.movement= touchMeta[index].lastMovement= {x: (this.position.x - touchMeta[index].lastPos.x),
+                                                           y: (this.position.y - touchMeta[index].lastPos.y)};
+        }else{
+            this.movement= touchMeta[index].lastMovement;
+            }
+        
+        this.speed= {x: (this.movement.x / this.timeout),
+                     y: (this.movement.y / this.timeout)};
+        
+        touchMeta[index].lastPos= this.position;
+        touchMeta[index].lastTime= now;
+        };
+
+    var OverScrollData= function(target, direction, y, speed){
+        this.cursorY= y;
+        this.cursorSpeed= speed;
+        this.lastUpdate= 0;
+        this.direction= direction;
+        this.target= target;
+        this.release= function(){
+            const backSpeed= 2000;
+            
+            var now= new Date().getTime();
+            var timeout= (now - this.lastUpdate) / 1000;
+            this.lastUpdate= now;
+            this.cursorY+= this.cursorSpeed * timeout;
+            var newStretch= stretching(this.cursorY);
+            
+            if(this.direction == OverScrollData.DOWN){
+                var stretchDelta= Math.abs(newStretch - parseInt(self.getComputedStyle(this.target.firstElementChild).marginTop.replace('px', '')));
+                var block= this.target.firstElementChild;
+                var margin= 'marginTop';
+            }else if(this.direction == OverScrollData.UP){
+                var stretchDelta= Math.abs(newStretch - parseInt(self.getComputedStyle(this.target.lastElementChild).marginBottom.replace('px', '')));
+                var block= this.target.lastElementChild;
+                var margin= 'marginBottom';
+                }
+            
+            if(this.cursorSpeed === 0 || stretchDelta <= 1){
+                this.cursorY-= backSpeed * timeout;
+                if(this.cursorY < 0) this.cursorY= 0;
+                self.console.log('cursorY: '+this.cursorY);
+                block.style[margin]= stretching(this.cursorY)+'px';
+                }else{
+                    block.style[margin]= newStretch;
+                    }
+            if(newStretch > 0.5){
+                var target= this; 
+                self.requestAnimationFrame(function(){ target.release(); });
+            }else{
+                block.style[margin]= '0';
+                overScrollMeta.splice(overScrollMeta.indexOf(this), 1);
+                }
+            };
+        };
+    OverScrollData.UP= '1';
+    OverScrollData.DOWN= '0'; 
+    
+/*  ---     Hooks   --- */
     
 //  global init hook
     var initHook= function(e, index){
         touchMeta[index]= {
             lastTime : (new Date().getTime()),
             lastPos : { x: e.screenX, y: e.screenY },
-            lastPosDelta : {x: 0, y: 0},
-            lastTarget : this
+            lastMovement : {x: 0, y: 0},
+            lastTimeout : 0
             };
         };
     
 //  global movment hook
     var movmentHook= function(e, index){
-        var now= new Date().getTime();
-        var timeD= (now - touchMeta[index].lastTime) / 1000;
-        
-        if(timeD === 0){
-            timeD= touchMeta[index].lastTimeDelta;
-        }else{
-            touchMeta[index].lastTimeDelta= timeD;
-        }
-        e.pos= { x: e.screenX, y: e.screenY };
-        
-        if(touchMeta[index].lastPos.y != e.pos.y && touchMeta[index].lastPos.x != e.pos.x){
-            e.posDelta= touchMeta[index].lastPosDelta= {x: (e.pos.x - touchMeta[index].lastPos.x),
-                                                        y: (e.pos.y - touchMeta[index].lastPos.y)};
-        }else{
-            e.posDelta= touchMeta[index].lastPosDelta;
-            }
-        
-        e.speed= {x: (e.posDelta.x / timeD),
-                  y: (e.posDelta.y / timeD)};
-        
-        self.console.log('target: '+this.className+'\nbefore Y: '+touchMeta[index].lastPos.y+'\nnow Y: '+e.pos.y+'\ndelta y: '+e.posDelta.y+'\ntime delta: '+timeD+'\nspeed y: '+e.speed.y);
-        
-        touchMeta[index].lastPos= e.pos;
-        touchMeta[index].lastTime= now;
-        
-        if(touchMeta[index].lastTarget != this) touchMeta[index].lastTarget= this;
-
+        EventObjectExtension.apply(e, [index]);
+                
 //      grabbed scroll
-        if( /*(touchMeta[index].lastTarget == this) &&*/ (self.getComputedStyle(this).getPropertyValue('overflow') == 'hidden') && (!this.hasAttribute('physicalTouch') || this.getAttribute('physicalTouch') != 'false') && (e.posDelta.y !== 0) ){
-            physicalScroll(this, e.posDelta.y, true);
+        if( (e.movement.y !== 0) && (self.getComputedStyle(this).getPropertyValue('overflow') == 'hidden') && (!this.hasAttribute('physicalTouch') || this.getAttribute('physicalTouch') != 'false') ){
+            physicalScroll(this, e.movement.y, true);
             }
         };
     
 //  global release hook
     var releaseHook= function(e, index){
-        var now= new Date().getTime();
-        var timeD= (now - touchMeta[index].lastTime) / 1000;
-        e.pos= { x: e.screenX, y: e.screenY };
-        e.posDelta= {x: Math.abs(e.pos.x - touchMeta[index].lastPos.x),
-                     y: Math.abs(e.pos.y - touchMeta[index].lastPos.y)};
-        
-        if(!(e.posDelta.x < 1 && e.posDelta.y < 1 && timeD < 0.1)){
-            e.speed= {x: (e.posDelta.x / timeD),
-                      y: (e.posDelta.y / timeD)};
-            }
+        EventObjectExtension.apply(e, [index]);
         
 //      released remaining velocity scroll
-        if( /*(touchMeta[index].lastTarget == e.target) &&*/ (self.getComputedStyle(this).getPropertyValue('overflow') == 'hidden') && (!this.hasAttribute('physicalTouch') || this.getAttribute('physicalTouch') != 'false')){
+        if( (self.getComputedStyle(this).getPropertyValue('overflow') == 'hidden') && (!this.hasAttribute('physicalTouch') || this.getAttribute('physicalTouch') != 'false')){
             physicalScroll(this, e.speed.y, false);
             }
         };
     
-//  physical scroll
+//  attaching all Hooks
     allElements.forEach(function(item){
+        item._physicalID= -1;
+        
         item.addEventListener(self.events.TOUCHSTART, function(e){
             if(self.navigator.isTouch){
                 var emitter= this;
@@ -127,12 +173,52 @@ $('escape').wrapper(function(){
         });
     
 /* ---      physical functions      --- */
-    var physicalScroll= function(target, change, isGrabbed){
-        change*= -1;
+    
+    var physicalScroll= function(target, movement, isGrabbed){
+        
+//      invert movement direction to scroll direction
+        var change= movement * -1;
+
+//      if the scrolled element is still grabbed
         if(isGrabbed){
             target.scrollTop+= change;
-        }else{
             
+//          on overscrolling at the top
+            if(target.scrollTop === 0 && movement > 0){
+                if(target._physicalID < 0 || !overScrollMeta[target._physicalID]){
+                    overScrollMeta.push(new OverScrollData(target, OverScrollData.DOWN, 0, 0));
+                    target._physicalID= overScrollMeta.length - 1;
+                    }
+                var sData= overScrollMeta[target._physicalID];
+                sData.cursorY+= Math.abs(change);
+                target.firstElementChild.style.marginTop= stretching(sData.cursorY)+'px';
+                target.scrollTop= 0;
+                
+//          on overscrolling at the bottom
+            }else if(target.scrollTop == target.scrollTopMax && movement < 0){
+                if(target._physicalID < 0 || !overScrollMeta[target._physicalID]){
+                    overScrollMeta.push(new OverScrollData(target, OverScrollData.UP, 0, 0));
+                    target._physicalID= overScrollMeta.length - 1;
+                    }
+                var sData= overScrollMeta[target._physicalID];
+                sData.cursorY+= Math.abs(change);
+                target.lastElementChild.style.marginBottom= stretching(sData.cursorY)+'px';
+                target.scrollTop= target.scrollTopMax;
+                }
+            
+//      if the object was released
+        }else{
+            if(target._physicalID > -1 && overScrollMeta[target._physicalID]){
+                var sData= overScrollMeta[target._physicalID];
+                sData.lastUpdate= new Date().getTime();
+                sData.release();
+                }
             }
+        };
+    
+    var stretching= function(F){
+        const D= 3;
+        
+        return F / D;
         };
 });
