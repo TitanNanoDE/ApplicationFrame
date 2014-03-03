65337fa3aa3eca78454251622335c2e760825945
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2014-03-03 15:55:30 +0100

    rework of the hash controler
    complete rework of the hash controler.
    The controler uses now the hash-bang and creates 'proper' URLs. support
    for the history.pushState API is not planned for this script but
    creating a location controler is possible...

--- modules/hashControl.js
+++ modules/hashControl.js
@@ -6,78 +6,115 @@ this.self= this, 'use strict';
     
 $('new')({
     name : 'hash',
-    construct : function(engine){
+    constructor : function(engine){
         engine.hash= {
             actions : [],
-            cleanUps : [],
-            items : []
+            path : []
         };
-        self.addEventListener('hashchange', function(){
-            var hash= self.location.hash.split('&');
-            for(var i= 0; i < hash.length; i++){
-                if(engine.hash.items.indexOf(hash[i]) < 0){
+        
+//      Classes
+        var HashEvent= function(type, path){
+            this.type= type;
+            this.path= path;
+            this.trigger= function(){
+                var path= this.path;
+                if(this.type == HashEvent.ADD){
                     engine.hash.actions.forEach(function(item){
-                        if(item.type instanceof Array)
-                            if(item.type.indexOf(hash[i]) > -1) item.action();
-                        else    
-                            if(item.type == hash[i]) item.action();
-                        });
-                    }
+                        if(item.path == path && item.enter)
+                            item.enter();
+                    });
+                }else if(this.type == HashEvent.LOST){
+                    engine.hash.actions.forEach(function(item){
+                        if(item.path == path && item.exit)
+                            item.exit();
+                    });
+                }else{
+                    self.console.error('unknown HashEvent type!');
                 }
-            engine.hash.items.forEach(function(item){
-                if(hash.indexOf(item) < 0){
-                    engine.hash.cleanUps.forEach(function(item){
-                        if(item.type instanceof Array)
-                            if(item.type.indexOf(hash[i]) > -1) item.action();
-                        else    
-                            if(item.type == hash[i]) item.action();                         
-                        });
-                    }
-                });
-            engine.hash.items= hash;
+            };
+        };
+        
+        HashEvent.ADD= 0;
+        HashEvent.LOST= 1;
+        
+        self.addEventListener('hashchange', function(){
+            var hashPath= self.location.hash.split('/');
+            
+//          check hash path
+            if(hashPath[0] != '#!'){
+                self.console.error('error in your hash path!'); 
+                return false;
+            }
+            hashPath.shift();
+            
+//          compare old and new paths
+            var events= [];
+//          find lost elements
+            var difference= false;
+            var path= '';
+            for(var i= 0; i < engine.hash.path; i++){
+                path+= '/' + engine.hash.path[i];
+                        
+                if(difference)
+                    events.push(new HashEvent(HashEvent.LOST, path));
+                else if(engine.hash.path[i] == hashPath[i])
+                    continue;
+                else if(engine.hash.path[i] != hashPath[i]){
+                    difference= true;
+                    events.push(new HashEvent(HashEvent.LOST, path));
+                }
+            }
+            events.reverse();
+//          find new elements
+            path= '';
+            difference= false;
+            for(var i= 0; i < hashPath.length; i++){
+                path+= '/' + hashPath[i];
+                
+                if(difference)
+                    events.push(new HashEvent(HashEvent.ADD, path));
+                else if(hashPath[i] == engine.hash.path[i])
+                    continue;
+                else if(hashPath[i] != engine.hash.path[i]){
+                    difference= true;
+                    events.push(new HashEvent(HashEvent.ADD, path));
+                }
+            }
+            
+            events.forEach(function(item){
+                item.trigger();
             });
-        var buildHash= function(hashArray){
-            var new_hash_string= '';
-            if(hashArray.length > 0){
-                new_hash_string+= hashArray[0];
-                for(var i=1; i < hashArray.length; i++){
-                    new_hash_string+= '&'+hashArray[i];
-                    }
-                self.location.hash= new_hash_string;
-                }
-            return new_hash_string;
-            };
-        this.on= function(type, action){
-            engine.hash.actions.push({type: type, action : action});
-            };
-        this.after= function(type, action){
-            engine.hash.cleanUps.push({type: type, action: action});
-            };
-        this.add= function(item){
-            var hash= self.location.hash.split('&');
-            hash.push(item);
-            self.location.hash= buildHash(hash);
-            };
-        this.replace= function(key, value){
-            var hash= self.location.hash.split('&');
-            for(var i=0; i < hash.length; i++){
-                if(hash[i].indexOf('_'+key+':') > -1){
-                    hash[i]= '_'+key+':'+value;
-                    self.location.hash= buildHash(hash);
-                    break;
-                    }
-                }
-            };
-        this.remove= function(item){
-            var hash= self.location.hash.split('&');
-            hash.splice(hash.indexOf(item), 1);
-            self.location.hash= buildHash(hash);
-            };
-        this.createItem= function(key, value){
-            return '_'+key+':'+value;
-            };
-        this.create= function(key){
-            self.hash+= '&_'+key+':null';
-            };
+            
+            engine.hash.path= hashPath;
+            
+//          Google Analytics Support (only analytics.js)
+            if(self.ga){
+                var location= self.location.protocol + '//' + self.location.hostname + self.location.pathname + self.location.search + self.location.hash;
+                self.ga('send', 'pageview', location);
+            }
+        });
+
+        this.mount= function(path, enter, exit){
+            return engine.hash.actions.push({path: path, enter : enter, exit: exit});
+        };
+        
+        this.unmount= function(id){
+            engine.hash.actions.splice(id, 1);
+        };
+        
+        this.down= function(newElement){
+            self.location.hash+= '/' + newElement;
+        };
+        
+        this.up= function(){
+            var hash= self.location.hash.split('/');
+            hash.shift();
+            hash.pop();
+            self.location.hash= '!/' + hash.join('/');
+        };
+        
+        this.swichTo= function(path){
+            self.location.hash= '!' + path;
+        };
     }
 });
