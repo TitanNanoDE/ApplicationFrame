9c13dbb5fd757a507989db45d930e35ca36cfd49
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2014-03-10 17:36:24 +0100

    swich to lf line ending

---
+++ modules/connections.js
@@ -0,0 +1,54 @@
+// connections.js v. 0.1 part of the Application Frame
+
+$('new')({
+    name :'connections',
+    object : {
+        classes : {
+            Server : function(type, url){
+                this._type= type;
+                this._url= url;
+                this._lastMessage= null;
+                this._open= false;
+                this._xhr= new self.XMLHttpRequest();
+            }
+        },
+        
+        statics : {
+            UPSTREAM_NORMAL : 0,
+            DOWNSTREAM_NORMAL : 1
+        }
+    },
+    _init : function(me){
+        me.classes.Server.prototype= {
+            get url(){
+                return this._url;
+            },
+            
+            get lastMessage(){
+                return this._lastMessage;
+            },
+            
+            get open(){
+                return this._open;
+            },
+            
+            push : function(message){
+                var me= this;
+                return new self.Promise(function(setVaule, setError){
+                    me._xhr.open('POST', this._url, true);
+                    me.send(message);
+                    me._xhr.onreadystatechange= function(e){
+                        if(e.readyState == 4){
+                            if(e.status == 200){
+                                setVaule(e);
+                            }else{
+                                setError(e);
+                            }
+                        me._lastMessage= e.statusText;
+                        }
+                    };
+                });
+            }
+        };
+    }
+});
