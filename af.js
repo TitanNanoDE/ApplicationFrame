ee94c3d8bebb4a9d03b2a30544c56cf4ce33cb6e
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2014-05-06 19:32:51 +0200

    rough implementation of services
    not sure if everything works well. Processing of the function-call stack is not implemented yet.

--- af.js
+++ af.js
@@ -39,12 +39,12 @@ var ApplicationScope= function(name){
 		allowGetters : true,
 		lockOverride : false,
 		autoLock : true
-        };
+    };
 	this.properties=  { 
         addSetterListener : function(key, callback){
 			thisScope.setterListeners.push({key : key, callback : callback});
-			}
-        };
+        }
+    };
 	this.thread= null;
 	this.worker= null;
 	this.setterListeners= [];
@@ -53,9 +53,9 @@ var ApplicationScope= function(name){
             for(var i in newSettings)
                 if(thisScope.settings[i]) thisScope.settings[i]= newSettings[i];
             if(!newSettings.hasOwnProperty('lockOverride') && thisScope.settings.autoLock) thisScope.settings.lockOverride= true;
-            }
-        };
-	};
+        }
+    };
+};
     
 var MozillaAddonScope = function(){
     var thisScope= this;
@@ -66,17 +66,91 @@ var MozillaAddonScope = function(){
         contentPrefixChar : '*',
         contentPrefix : false,
         fastExport : true
-        };
-    this.defaultProperties= {
-        };
+    };
+    this.defaultProperties= {};
     this.modules= {};
     this.global= null;
     this.thread= null;
     this.override= function(newSettings){
         for(var i in newSettings)
             if(thisScope.settings[i]) thisScope.settings[i]= newSettings[i];
-        };
     };
+};
+    
+var ServiceScopeLocal= function(){
+    var thisScope= this;
+    this.name= 'serviceLocal';
+    this.type= 'service';
+    this.settings= {
+        allowSubWorkers : true,
+        lockOverride : false
+    };
+    this.properties= {
+        recive : function(name, callback){
+            self.addEventListener('message', function(e){
+                if(e.data.name == name){
+                    var id= e.data.id;
+                    var setAnswert= function(data){
+                        self.postMessage({
+                            name : id,
+                            data : data
+                        });
+                    };
+                    callback(e.data, setAnswert);
+                }
+            }, false);
+        }
+    };
+    this.thread= null;
+    this.override= function(newSettings){
+        if(!thisScope.settings.lockOverride){
+            for(var i in newSettings)
+                if(this.settings[i] && !this.settings.lockOverride) thisScope.settings[i]= newSettings[i];
+        }
+    };
+};
+    
+var ServiceScopeRemote= function(name){
+    this.name= name;
+    this.type= 'serviceRemote';
+    this.thread= null;
+    this.isReady= false;
+};
+ServiceScopeRemote.prototype= {
+    push : function(message){
+        var scope= this;
+        return new self.Promise(function(setSuccess){
+            var id= Date.now();
+            message.id= id;
+            var listener= function(e){
+                if(e.data.name == id){
+                    this.removeEventListener('message');
+                    setSuccess(e.data);
+                }
+            };
+            scope.thread.addEventListener('message', listener, false);
+            scope.thread.postMessage(message);
+        });
+    },
+    pushStack : function(stack){
+        return this.push({
+            name : 'transferStack',
+            data : stack
+        });
+    }
+};
+    
+var WorkerStack= function(options){
+    this.action= null;
+    this.settings= null;
+    this.arguments= null;
+    this.call= null;
+    this.source= null;
+    for(var i in options){
+        if(i in this)
+            this[i]= options[i];
+    }
+};
 	
 var scopes= [];
 
@@ -95,11 +169,27 @@ var items= {
 				if(settings.singleApplicationMode) engine.isApplicationLimit= true;
                 if(settings.masterApplication == scopes[scopes.length-1].name) 
                     engine.mainApplication= scopes[scopes.length-1];
-			}else{
-				throw "application \""+name+"\" already exists!!";
-				}
+			}else
+                self.console.log("application or service \""+name+"\" already exists!!");
+				
 			}
 		},
+    service : {
+        'new' : function(name){
+            var exists= false;
+            scopes.forEach(function(item){
+                if(item.name == name) exists= true;
+            });
+            
+            if(!exists){
+                scopes.push(new ServiceScopeRemote(name));
+            }else
+                self.console.log("application or service \""+name+"\" already exists!!");
+        },
+        setEngine : function(src){
+            engine.workerEngineSource= src;
+        }
+    },
     'new' : function(settings){
         var object = {};
         if(settings.constructor){
@@ -270,6 +360,51 @@ var prepareScope= function(item){
                     return self.require('sdk/self').data.url(name);
                 }
             };
+        }else if(item.type == 'serviceRemote'){
+            var scope= item;
+            return {
+                main : function(source){
+                    scope.thread= new self.Worker(engine.workerEngineSource);
+                    var source= 'self.__main__= ' + source.toString();
+                    source= new self.Blob([source], { type : 'text/javascript' });
+                    var firstStage= new WorkerStack({
+                        action : 'initThread',
+                        settings : scope.settings,
+                        source : self.URL.createObjectURL(source)
+                    });
+                    scope.pushStack(firstStage).then(function(){
+                        scope.isReady= true;
+                    });
+                },
+                call : function(name, argumentS){
+                    if(scope.isReady){
+                        return new self.Promise(function(setSuccess){
+                            scope.callStack({
+                                action : 'callFunction',
+                                call : name,
+                                arguments : argumentS
+                            }).then(function(answert){
+                                setSuccess(answert.functionResult);
+                            }); 
+                        });
+                    }
+                },
+                recive : function(name, callback){
+                    var listener= function(e){
+                        if(e.data.name == name){
+                            scope.thread.removeEventListener('message', listener, false);
+                            var setAnswert= function(message){
+                                scope.thread.postMessage({
+                                    name : e.data.id,
+                                    data : message
+                                });
+                            callback(e.data.data, setAnswert);
+                            };
+                        }
+                    };
+                    scope.thread.addEventListener('message', listener, false);
+                }
+            };
         }else 
             return null;
     }else{
@@ -338,6 +473,7 @@ var engine = {
     arch : 'x32',
     type : 'Web',
 	activeThreads : 0,
+    workerEngineSource : null,
 	domUpdates : [],
 	threadQueue : {
 		queue : [],
@@ -358,7 +494,8 @@ var engine = {
             }
         },
 		isRunning : false 
-		},
+    },
+    pushListeners : [],
     launchQueue : [],
     exit : function(){}
 	};
@@ -367,7 +504,7 @@ var engine = {
 var platform= null;    
 
 // find out which engine is used
-if (self.navigator){
+if (self.navigator && !self.importScripts){
     platform= self.navigator.userAgent;
 //              Mozilla
     platform=   (((platform.indexOf('Gecko/') > -1) && 'Gecko '+platform.substring(platform.indexOf('rv:')+3, platform.indexOf(')')) ) || false) ||
@@ -380,6 +517,20 @@ if (self.navigator){
     platform= platform.split(' ');
     platform.push('Web');
 
+// check if we are in a Worker
+}else if(self.navigator && self.importScripts){
+    platform= self.navigator.userAgent;
+//              Mozilla
+    platform=   (((platform.indexOf('Gecko/') > -1) && 'Gecko '+platform.substring(platform.indexOf('rv:')+3, platform.indexOf(')')) ) || false) ||
+//              Google / Apple / Opera
+                (((platform.indexOf('AppleWebKit') > -1) && platform.substring(platform.indexOf('AppleWebKit'), platform.lastIndexOf('(')-1).replace(/\//, ' ')) || false) ||      
+//              Microsoft
+                (((platform.indexOf('Trident/') > -1) && platform.substring(platform.indexOf('Trident/'), platform.indexOf(';', platform.indexOf('Trident/'))).replace(/\//, ' ')) || false) ||
+//              Unknown
+                'unknown platform';
+    platform= platform.split(' ');
+    platform.push('Worker');
+    
 // check if current platform is the Mozilla Add-on runtime
 }else if(self.exports && self.require && self.module){
     var system= self.require('sdk/system');
@@ -434,6 +585,10 @@ if(platform[2] == 'Web'){
 //        spread : (function(){try{ return eval("var x; x= [1, 2, 3], (function(x, y, z){})(...x), true;"); }catch(e){ return false; }})()
     };
     
+}else if(platform[2] == 'Worker'){
+//  at the moment there are no known platform tests for the 'Worker' platform.
+    var platformTests= {};
+    
 // Tests for the Mozilla Add-on SDK
 }else if(platform[2] == 'MozillaAddonSDK'){
 //  at the moment there are no known platform tests for the 'Mozilla Add-on SDK' platform.
@@ -458,22 +613,43 @@ for(var i in platformTests){
 
 //if a test failed the engine will quit
 if(faildTests > 0){
-    self.console.log(faildTests+' of '+allTests+' platform tests are faild!');
+    self.console.warn(faildTests+' of '+allTests+' platform tests are faild!');
 //    engine.exit(0);
 }
     
 // setup environment
-if(platform[2] == 'Web'){
+if(platform[2] == 'Web' || platform[2] == 'Worker'){
 //  check if touchscreen is supported
     self.navigator.isTouch= 'ontouchstart' in self;
     engine.name= platform[0];
     engine.version= platform[1];
     engine.platform= self.navigator.userAgent.substring(self.navigator.userAgent.indexOf('(')+1, self.navigator.userAgent.indexOf(';'));
     engine.arch= self.navigator.userAgent.substring(self.navigator.userAgent.indexOf(';')+2, self.navigator.userAgent.indexOf(';', self.navigator.userAgent.indexOf(';')+1));
-
+    engine.type= 'Web';
+    
 //  publish APIs
     self.$= selector;
     self.$_= scopeSelector;
+
+// worker additions
+    if(platform[2] == 'Worker'){
+        engine.type= 'Worker';
+        var scope= new ServiceScopeLocal();
+        scopes.push(scope);
+        scope.properties.recive('transferStack', function(e, setAnswert){
+            if(e.data.action == 'initThread'){
+                self.importScripts(e.data.source);
+                $_('engine').override(e.data.settings);
+                scope.thread= self.__main__;
+                engine.threadQueue.push(scope);
+                self.console.log('sending answert...');
+                setAnswert({
+                    status : true,
+                    statusText : 'success'
+                });
+            }
+        });
+    }
     
 }else if(platform[2] == 'MozillaAddonSDK'){
 //  create new Addon Scope
@@ -481,6 +657,7 @@ if(platform[2] == 'Web'){
     engine.name= platform[0];
     engine.version= platform[1];
     engine.platform= platform[2];
+    engine.type= 'MozAddon';
 //    self.require('af/addonCore.js'); // <--- does not exist yet. Not sure if it is really needed
     
 //  publish APIs
@@ -492,6 +669,7 @@ if(platform[2] == 'Web'){
     engine.version= platform[1];
     engine.platform= self.process.platform;
     engine.arch= self.process.arch;
+    engine.type= 'Node';
 
 //  publish APIs
     self.$= selector;
