d25c473180977312785dffbbbc2bd63ae4d194a2
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2014-09-28 18:52:31 +0200

    service loader for 0.2 services #8

---
+++ modules/ServiceLoader.js
@@ -0,0 +1,33 @@
+this.$$= $$;
+
+var createUniqueId= function(){
+	var time = Date.now();
+	while (time == Date.now());
+	return Date.now();
+};
+
+$$.listen= function(name, callback){
+	$$.addEventListener('message', function(e){
+		if(e.data.name == name){
+			var id= e.data.id;
+            var setAnswer= function(data){
+            	$$.postMessage({ name : id, data : data });
+			};
+			callback(e.data.data, setAnswer);
+		}
+	}, false);
+};
+
+$$.talk= function(name, data){
+	return new $$.Promise(function(success){
+		var id= createUniqueId();
+		var listener= function(e){
+			if(e.data.name == id){
+				$$.removeEventListener('message', listener);
+				success(e.data.data);
+			}
+		};
+		$$.addEventListener('message', listener, false);
+		$$.postMessage({ name : name, id : id, data : data });
+	});
+};
