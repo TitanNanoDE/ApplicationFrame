3f95e29bf76a7348edf185e94295d06994944579
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2015-01-05 22:00:08 +0100

    improved service loader

--- modules/ServiceLoader.js
+++ modules/ServiceLoader.js
@@ -1,33 +1,38 @@
-this.$$= $$;
+this.$$= this;
 
-var createUniqueId= function(){
-	var time = Date.now();
-	while (time == Date.now());
-	return Date.now();
-};
+(() => {
 
-$$.listen= function(name, callback){
-	$$.addEventListener('message', function(e){
-		if(e.data.name == name){
-			var id= e.data.id;
-            var setAnswer= function(data){
-            	$$.postMessage({ name : id, data : data });
-			};
-			callback(e.data.data, setAnswer);
-		}
-	}, false);
-};
+	'use strict';
 
-$$.talk= function(name, data){
-	return new $$.Promise(function(success){
-		var id= createUniqueId();
-		var listener= function(e){
-			if(e.data.name == id){
-				$$.removeEventListener('message', listener);
-				success(e.data.data);
+	var createUniqueId= function(){
+		var time = Date.now();
+		while (time == Date.now());
+		return Date.now();
+	};
+
+	$$.listen= function(name, callback){
+		$$.addEventListener('message', function(e){
+			if(e.data.name == name){
+				var id= e.data.id;
+				var setAnswer= function(data){
+					$$.postMessage({ name : id, data : data });
+				};
+				callback(e.data.data, setAnswer);
 			}
-		};
-		$$.addEventListener('message', listener, false);
-		$$.postMessage({ name : name, id : id, data : data });
-	});
-};
+		}, false);
+	};
+
+	$$.talk= function(name, data){
+		return new $$.Promise(function(success){
+			var id= createUniqueId();
+			var listener= function(e){
+				if(e.data.name == id){
+					$$.removeEventListener('message', listener);
+					success(e.data.data);
+				}
+			};
+			$$.addEventListener('message', listener, false);
+			$$.postMessage({ name : name, id : id, data : data });
+		});
+	};
+})();
