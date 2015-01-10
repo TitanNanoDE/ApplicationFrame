830092067f2ee4c5f6745c5d38592308196ab8fc
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2015-01-10 15:31:05 +0100

    improved core-extension

--- modules/core-extensions.js
+++ modules/core-extensions.js
@@ -1,18 +1,25 @@
 //default core extensions for the default Application Frame modules - copyright by TitanNano / Jovan Gerodetti - http://www.titannano.de
 
-$('wrap')(function(){
-    
-"use strict";
+var apply= function(){
+	"use strict";
 
-// if your required list type isn't here just add it
-if($$.NodeList && !$$.NodeList.prototype.forEach) $$.NodeList.prototype.forEach= $$.Array.prototype.forEach;
-if($$.TouchList && !$$.TouchList.prototype.forEach) $$.TouchList.prototype.forEach= $$.Array.prototype.forEach;
+// 	if your required list type isn't here just add it
+	if($$.NodeList && !$$.NodeList.prototype.forEach) $$.NodeList.prototype.forEach= $$.Array.prototype.forEach;
+	if($$.TouchList && !$$.TouchList.prototype.forEach) $$.TouchList.prototype.forEach= $$.Array.prototype.forEach;
 	
 /* --- last extension --- */
-var ArrayLast= function(){
-	return this[this.length -1];
+	var ArrayLast= function(){
+		return this[this.length -1];
+	};
+
+	if($$.Array && !$$.Array.prototype.last) $$.Array.prototype.last= ArrayLast;
+
+	if($$.TouchList && !$$.TouchList.prototype.indexOf) $$.TouchList.prototype.indexOf= Array.prototype.indexOf;
+	if($$.TouchList && !$$.TouchList.prototype.find) $$.TouchList.prototype.find= Array.prototype.find;
 };
-	
-if($$.Array && !$$.Array.prototype.last) $$.Array.prototype.last= ArrayLast;
-    
-});
+
+export default {
+	coreExtension : {
+		apply : apply
+	}
+};
