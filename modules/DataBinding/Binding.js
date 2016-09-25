c93c7472e3c4a9ac7e2893e4d2f236f75cfeff9b
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2016-09-25 16:11:53 +0200

    add current element to expression scope and improve text escaping

--- modules/DataBinding/Binding.js
+++ modules/DataBinding/Binding.js
@@ -51,32 +51,37 @@ let Binding = {
      */
     update : function(scope){
         let text = this.originalNodeValue;
+        let localNode = { element: this.parentNode };
         let values = this.properties.map(key => {
-            let item = { name : key, value : parseExpression(key, scope) };
+            let item = { name : key, value : parseExpression(key, localNode, scope) };
 
             return item;
         });
 
         if (this.singleExpression) {
-                text = parseExpression(text, scope);
+                text = parseExpression(text, localNode, scope);
         } else {
             values.forEach(pair => {
                 text = text.replace(`\{\{${pair.name}\}\}`, pair.value);
             });
         }
 
+        text = text.toString().trim().split(/\s+/).join(' ');
+
         if (hasPrototype(this.node, window.Attr)) {
-            if (polyInvoke(this.parentNode).getAttribute(this.node.name) !== text) {
+            if (this.parentNode.getAttribute(this.node.name) !== text) {
                 polyInvoke(this.parentNode).setAttribute(this.node.name, text);
             }
         } else {
-            text = text.toString().replace(/ /g, '\u00a0');
+            text = text.toString().replace(/\&nbsp\;/g, '\u00a0');
 
             if (this.node.textContent !== text) {
                 this.node.textContent = text;
             }
         }
-    }
+    },
+
+    test: function() { return true; }
 };
 
 export default Binding;
