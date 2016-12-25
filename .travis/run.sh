ffb039dece67e08d3de0bec3c771e140952cc071
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2016-12-25 18:19:57 +0100

    set node env to test

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha ./.travis/main.js --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
+NODE_ENV=test ./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha ./.travis/main.js --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
