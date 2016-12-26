799d046e4cf1a2228fd4d1eb52a5e62e93768015
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2016-12-26 18:59:50 +0100

    changed paths to test executables

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-NODE_ENV=test ./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha ./.travis/main.js --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
+NODE_ENV=test node_modules/istanbul/lib/cli.js cover ./node_modules/mocha/bin/_mocha ./.travis/main.js --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
