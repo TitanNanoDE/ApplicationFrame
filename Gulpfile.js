26d15c91959f924e9777c4a678491bef02cd818b
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2016-12-28 23:01:58 +0100

    we don't need testables for files we don't want to test

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@
 
 let gulp = require('gulp');
 let babel = require('gulp-babel');
+const clean = require('gulp-clean');
 let merge = require ('merge-stream');
 
 const dist = 'testable/';
@@ -10,22 +11,20 @@ const babelConfig = {
     "plugins": ["babel-plugin-transform-es2015-modules-commonjs"]
 };
 
-gulp.task('default', () => {
-    let main = gulp.src(['af.js'])
-        .pipe(babel(babelConfig))
-        .pipe(gulp.dest(dist));
+gulp.task('clean', () => {
+    return gulp.src(dist, { read: false })
+        .pipe(clean());
+})
+
+gulp.task('default', ['clean'], () => {
 
     let core = gulp.src(['core/**/*.js'])
         .pipe(babel(babelConfig))
         .pipe(gulp.dest(dist + 'core/'));
 
-    let modules = gulp.src(['modules/**/*.js'])
-        .pipe(babel(babelConfig))
-        .pipe(gulp.dest(dist + 'modules'));
-
     let util = gulp.src(['util/**/*.js'])
         .pipe(babel(babelConfig))
         .pipe(gulp.dest(dist + 'util'));
 
-    return merge(main, core, modules, util);
+    return merge(core, util);
 });
