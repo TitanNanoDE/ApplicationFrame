e56751b03feb003d648d374fec93453211107b39
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2017-03-14 23:08:41 +0100

    accept all 2xx responses

--- core/NetworkRequest.js
+++ core/NetworkRequest.js
@@ -11,23 +11,23 @@
  * @return {Object} the initial object
  */
 let stripHashKey = function(object){
-	if (Array.isArray(object)) {
-		object = object.map(stripHashKey);
+    if (Array.isArray(object)) {
+        object = object.map(stripHashKey);
 
-	} else {
-		object = JSON.parse(JSON.stringify(object));
+    } else {
+        object = JSON.parse(JSON.stringify(object));
 
-		Object.keys(object).forEach(function(key){
-			if (key == '$$hashKey') {
-				delete object[key];
-			}else if (typeof object[key] === 'object' ) {
-				object[key] = stripHashKey(object[key]);
-			}
-		});
-	}
+        Object.keys(object).forEach((key) => {
+            if (key == '$$hashKey') {
+                delete object[key];
+            }else if (typeof object[key] === 'object' ) {
+                object[key] = stripHashKey(object[key]);
+            }
+        });
+    }
 
-	return object;
-}
+    return object;
+};
 
 /**
  * @lends module:NetworkRequest.NetworkRequest#
@@ -43,22 +43,22 @@ let NetworkRequest = {
      * @private
      * @type {Object}
      */
-	_headers : null,
+    _headers : null,
 
     /**
      * @type {string}
      */
-	type : '',
+    type : '',
 
     /**
      * @type {string}
      */
-	method : '',
+    method : '',
 
     /**
      * @type {string}
      */
-	url : '',
+    url : '',
 
     /**
      * @type {function[]}
@@ -76,13 +76,13 @@ let NetworkRequest = {
 	 *
 	 * @return {NetworkRequest} the request it self
 	 */
-	_make : function(url, { method = 'GET', type = 'none' } = {}){
-		this.type = type;
-		this.method = method;
+    _make : function(url, { method = 'GET', type = 'none' } = {}){
+        this.type = type;
+        this.method = method;
         this._headers = {};
-		this.url = url;
+        this.url = url;
         this._listeners = [];
-	},
+    },
 
 	/**
 	 * this method will set the given object as the request body.
@@ -91,11 +91,11 @@ let NetworkRequest = {
 	 *
 	 * @return {NetworkRequest} the request it self
 	 */
-	body : function(data){
-		this._body = data;
+    body : function(data){
+        this._body = data;
 
-		return this;
-	},
+        return this;
+    },
 
 	/**
 	 * This method will set the request headers, in case custom headers are required.
@@ -104,11 +104,11 @@ let NetworkRequest = {
 	 *
 	 * @return {NetworkRequest} the request it self
 	 */
-	headers : function(headers) {
-		this._headers = headers;
+    headers : function(headers) {
+        this._headers = headers;
 
-		return this;
-	},
+        return this;
+    },
 
     /**
      * Sets a single header for this request.
@@ -140,59 +140,59 @@ let NetworkRequest = {
 	 *
 	 * @return {Promise} resolves when the request is done
 	 */
-	send : function(){
-		let self = this;
-		let xhr = new XMLHttpRequest();
+    send : function(){
+        let self = this;
+        let xhr = new XMLHttpRequest();
 
-		if (this.method === 'GET' && this._body) {
-			this.url += '?' + Object.keys(this._body).map(function(key){
-				return `${key}=${self._body[key]}`;
-			}).join('&');
-		}
+        if (this.method === 'GET' && this._body) {
+            this.url += '?' + Object.keys(this._body).map((key) => {
+                return `${key}=${self._body[key]}`;
+            }).join('&');
+        }
 
-		xhr.open(this.method, this.url, true);
+        xhr.open(this.method, this.url, true);
 
-		let promise = new Promise((success, failure) => {
-			xhr.onreadystatechange = () => {
-				if (xhr.readyState === 4) {
-					if (xhr.status === 200) {
-						let response = xhr.response;
+        let promise = new Promise((success, failure) => {
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    if (xhr.status >= 200 && xhr.status < 300) {
+                        let response = xhr.response;
 
-						if (xhr.getResponseHeader('Content-Type').indexOf('application/json') > -1 && typeof response  === 'string') {
+                        if (xhr.getResponseHeader('Content-Type').indexOf('application/json') > -1 && typeof response  === 'string') {
                             response = JSON.parse(response);
-						}
+                        }
 
                         this._listeners.forEach(fn => fn(xhr));
 
-						success(response);
-					} else {
-						failure(xhr);
-					}
-				}
-			}
-		})
+                        success(response);
+                    } else {
+                        failure(xhr);
+                    }
+                }
+            };
+        });
 
-		Object.keys(this._headers).forEach(function(key){
-			xhr.setRequestHeader(key, self._headers[key]);
-		});
+        Object.keys(this._headers).forEach((key) => {
+            xhr.setRequestHeader(key, self._headers[key]);
+        });
 
-		if (this.type === 'json') {
+        if (this.type === 'json') {
             let body = this._body;
 
-			xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('Content-Type', 'application/json');
 
             if (body){
                 body = stripHashKey(body);
                 body = JSON.stringify(body);
             }
 
-			xhr.send(body);
-		} else {
-			xhr.send(this._body);
-		}
+            xhr.send(body);
+        } else {
+            xhr.send(this._body);
+        }
 
-		return promise;
-	}
+        return promise;
+    }
 };
 
 export default NetworkRequest;
