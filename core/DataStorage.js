bcc3d576bcceec974f684714fb9a41515feb0660
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2017-09-27 22:53:21 +0800

    fix DataStorage

--- core/DataStorage.js
+++ core/DataStorage.js
@@ -1,43 +1,67 @@
 import EventTarget from './EventTarget';
 
-const whenFilled = function(successCallback) {
-    if (successCallback && typeof successCallback === 'function') {
-        this._filledCallbacks.push({ once: false, callback: successCallback });
-    }
-
-    if (this._value) {
-        successCallback(this._value);
-    }
-};
-
-const whenNext = function(callback) {
-    if (callback && typeof callback === 'function') {
-        this._filledCallbacks.push({ once: true, callback: callback });
-    }
-};
-
-const once = function(callback) {
-    if (callback && typeof callback === 'function') {
-        if (!this._value) {
-            this._filledCallbacks.push({ once: true, callback: callback });
-        } else {
-            callback(this._value);
-        }
-    }
-};
-
 // fake then if this should be handed to something that expects a promise
-whenFilled.then = whenNext.then = once.then = function(callback) {
+const thenHandler = function(callback) {
     return (new Promise((done) => {
         this(done);
     })).then(callback);
 };
 
 // dummy catch in case someone tries to use it
-whenFilled.catch = whenNext.catch = once.catch = function(callback) {
+const catchHandler = function(callback) {
     return (new Promise((done) => {
         this(done);
     })).catch(callback);
+};
+
+const whenFilled = function(target) {
+    let f = function(successCallback) {
+        if (successCallback && typeof successCallback === 'function') {
+            this._filledCallbacks.push({ once: false, callback: successCallback });
+        }
+
+        if (this._value) {
+            successCallback(this._value);
+        }
+    };
+
+    f = f.bind(target);
+    f.then = thenHandler;
+    f.catch = catchHandler;
+
+    return f;
+};
+
+const whenNext = function(target) {
+    let f = function(callback) {
+        if (callback && typeof callback === 'function') {
+            this._filledCallbacks.push({ once: true, callback: callback });
+        }
+    };
+
+    f = f.bind(target);
+    f.then = thenHandler;
+    f.catch = catchHandler;
+
+    return f;
+};
+
+const once = function(target) {
+    let f = function(callback) {
+        if (callback && typeof callback === 'function') {
+            if (!this._value) {
+                this._filledCallbacks.push({ once: true, callback: callback });
+            } else {
+                callback(this._value);
+            }
+        }
+    };
+
+    f = f.bind(target);
+    f.then = thenHandler;
+    f.catch = catchHandler;
+
+    return f;
 };
 
 const DataStorage = {
@@ -49,9 +73,11 @@ const DataStorage = {
     },
 
     constructor() {
-        this.when = whenFilled.bind(this);
-        this.whenNext = whenNext.bind(this);
-        this.once = once.bind(this);
+        super.constructor();
+
+        this.when = whenFilled(this);
+        this.whenNext = whenNext(this);
+        this.once = once(this);
         this._filledCallbacks = [];
 
         return this;
