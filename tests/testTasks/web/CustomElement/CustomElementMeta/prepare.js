8ae123c3486ba32b440b3353c5110ce558f433af
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2021-04-27 20:26:45 +0200

    [Web] reflectChanges on CustomElement attribute causes infinite loop #140 (#146)

--- tests/testTasks/web/CustomElement/CustomElementMeta/prepare.js
+++ tests/testTasks/web/CustomElement/CustomElementMeta/prepare.js
@@ -1,7 +1,8 @@
-/* globals testContext, CustomElementMeta */
+/* globals testContext, CustomElementMeta, CustomElement */
 
 (() => {
     testContext.metaObject = Object.assign({ __proto__: CustomElementMeta }, testContext.meta);
+    testContext.prototype = Object.assign({ __proto__: CustomElement }, testContext.prototype);
 
     if (testContext.onPropertyChanged) {
         testContext.prototype[testContext.metaObject.symbols.onPropertyChanged] = testContext.onPropertyChanged;
